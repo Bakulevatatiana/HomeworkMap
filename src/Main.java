2f0fd1ba8d9ae758d6e7242b6610c0a37234cf11
bakuleva <t.kovantseva@mail.ru>
2023-02-07 09:40:37 +0700

    ДЗ сделанно

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class Main {
+    private final static Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addPhone(new FullName("Test", "Test"), "89234768880");
+        phoneBook.addPhone(new FullName("Test1", "Test1"), "89234768880");
+        while (phoneBook.getEntries().size()<20){
+            phoneBook.addPhone(getRandomName(),getPhoneNumbers());
+        }
+        for(Map.Entry<FullName, String> entry: phoneBook.getEntries()){
+            System.out.printf("%s-%s%n",entry.getKey(), entry.getValue());
+
+        }
+    }
+
+
+
+    public static FullName getRandomName() {
+        String name = "Test"+ RANDOM.nextInt(1_000_000);
+        String lastName = "TestLast"+ RANDOM.nextInt( 1_000_000);
+        return new FullName(name,lastName);
+
+    }
+    public static String getPhoneNumbers(){
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i = 0; i <11; i++) {
+            stringBuilder.append(RANDOM.nextInt(1_000_000));
+            
+        }
+        return stringBuilder.toString();
+    }
+
+
+}
